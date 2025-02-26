c52b9c01a1ebedd35b32deff9c70121936c5c133
jostlebot <jocelyn.skillman@gmail.com>
2025-02-25 16:59:06 -0800

    Update GuessWhoFromLuke.py

--- GuessWhoFromLuke.py
+++ GuessWhoFromLuke.py
@@ -1,9 +1,6 @@
 import os
 import openai
 
-# Use the globally set environment variable for the API key
-openai.api_key = os.getenv('OPENAI_API_KEY')
-
 response = openai.ChatCompletion.create(
     model="gpt-4-turbo",
     messages=[
